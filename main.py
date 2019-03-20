50a5cb5c31780756c5c54d543786eb58b3e37b28
Cory Forsstrom <cforsstrom18@gmail.com>
2019-03-20 00:01:22 +0000

    init

---
+++ main.py
@@ -0,0 +1,70 @@
+import time
+from datetime import datetime
+import pytz
+from flask import Flask, request, render_template, jsonify
+from threading import Thread
+from queue import Queue
+
+TEMP_TIMEOUT = 60*60
+LOCAL_TZ = pytz.timezone('US/Pacific')
+
+queue = Queue()
+
+app = Flask(__name__)
+
+temps = [{'id': 'temp', 'temp1': 0, 'temp2': 0, 'volts': 0, 'date': datetime.now(LOCAL_TZ)}]
+
+@app.route('/')
+def index():
+    return render_template('index.html', temps=temps)
+
+@app.route('/api/temp', methods=['GET', 'POST'])
+def api_temp():
+    if request.method == 'POST':
+        print(request.mimetype)
+        if request.is_json:
+            json = request.get_json(silent=True)
+            print(json)
+            if json is None:
+                response = { 'status': 'failure', 'error_message': 'Json empty or not valid' }
+                return jsonify(response)
+            queue.put(time.time())
+            if temps[0]['id']=='temp':
+                temps.pop(0)
+            temps.append({'id': len(temps), 'temp1': json['Temp1'],
+                          'temp2': json['Temp2'], 'volts': json['Volts'],
+                          'date': datetime.now(LOCAL_TZ)})
+            response = { 'status': 'success' }
+            return jsonify(response)
+        else:
+            response = { 'status': 'failure', 'error_message': 'Must be application/json' }
+            return jsonify(response)
+    else:
+        response = { 'status': 'success', 'data': temps }
+        return jsonify(response)
+
+@app.route('/api/temp/<int:id>', methods=['GET'])
+def api_temp_id(id):
+    try:
+        temp = [temp for temp in temps if temp['id'] == id][0]
+        response = {'status': 'succcess', 'data': temp}
+    except:
+        response = {'status': 'failure', 'error_message': 'Invalid id' }
+    return jsonify(response)
+
+def temp_timeout(queue):
+    last_update = time.time()
+    while True:
+        try:
+            last_update = queue.get(timeout=11)
+        except:
+            pass
+        delta = time.time() - last_update
+        print('Seconds since last temp update: {}'.format(delta))
+        if delta > TEMP_TIMEOUT:
+            temps.clear()
+            temps.append({'id': 'temp', 'temp1': 0, 'temp2': 0, 'volts': 0, 'date': datetime.now(LOCAL_TZ)})
+
+if __name__ == '__main__':
+    Thread(target=temp_timeout, args=(queue,)).start()
+    app.run(host='0.0.0.0', port=8085)
