739b06101c291c435a48881f410a5b5d85425693
Cory <cforsstrom18@gmail.com>
2019-03-22 12:44:49 -0700

    sessh and ids all working!

--- bbq/models.py
+++ bbq/models.py
@@ -20,29 +20,57 @@ class Device(Base):
     mac = Column(String, unique=True)
     created = Column(DateTime, nullable=False, default=utcnow())
     last_modified = Column(DateTime, onupdate=utcnow())
-    temps = relationship("Temp", primaryjoin="Temp.device_id==Device.id")
+    sessions = relationship("Session", primaryjoin="Session.device_id==Device.id")
 
     def __init__(self, mac=None):
         self.mac = mac
 
     def __repr__(self):
-        return '<Device {}>'.format(self.mac)
+        return '<Device {} - {}>'.format(self.id, self.mac)
+
+
+class Session(Base):
+    __tablename__ = 'sessions'
+    id = Column(Integer, primary_key=True)
+    device_session_id = Column(Integer)
+    device_id = Column(Integer, ForeignKey('devices.id'), nullable=False)
+    device = relationship(Device, primaryjoin=device_id == Device.id)
+    temps = relationship("Temp", primaryjoin="Temp.session_id==Session.id")
+    created = Column(DateTime, nullable=False, default=utcnow())
+    last_modified = Column(DateTime, onupdate=utcnow())
+
+    def __init__(self, Device=None):
+        self.device_id = Device.id
+        self.device_session_id = len(Device.sessions)+1
+
+    def __repr__(self):
+        return '<Session {}/{} - Device {}>'.format(self.id, self.device_session_id, self.device.id)
 
 
 class Temp(Base):
     __tablename__ = 'temps'
     id = Column(Integer, primary_key=True)
+    sesssion_temp_id = Column(Integer)
     temp_1 = Column(Integer, nullable=False)
     temp_2 = Column(Integer, nullable=False)
     volts = Column(Float, nullable=False)
-    device_id = Column(Integer, ForeignKey('devices.id'), nullable=False)
-    device = relationship(Device, primaryjoin=device_id == Device.id)
+    mode = Column(String)
+    runtime_seconds = Column(Integer)
+    session_id = Column(Integer, ForeignKey('sessions.id'), nullable=False)
+    session = relationship(Session, primaryjoin=session_id == Session.id)
+    created = Column(DateTime, nullable=False, default=utcnow())
+    last_modified = Column(DateTime, onupdate=utcnow())
 
-    def __init__(self, device_id=None, temp_1=None, temp_2=None, volts=None):
-        self.device_id = device_id
+    def __init__(self, Session=None, temp_1=None, temp_2=None, volts=None, **kwargs):
+        self.session_id = Session.id
+        self.sesssion_temp_id = len(Session.temps)+1
         self.temp_1 = temp_1
         self.temp_2 = temp_2
         self.volts = volts
+        if 'mode' in kwargs:
+            self.mode = kwargs['mode']
+        if 'runtime_seconds' in kwargs:
+            self.runtime_seconds = kwargs['runtime_seconds']
 
     def __repr__(self):
-        return '<Temp {} - {}, {}>'.format(self.device.mac, self.temp_1, self.temp_2)
+        return '<Temp {}/{} - Session {}>'.format(self.id, self.sesssion_temp_id, self.session_id)
