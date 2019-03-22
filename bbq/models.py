f945bc1ac968b43dec6c44150ae7417d123055c7
Cory Forsstrom <cforsstrom18@gmail.com>
2019-03-22 00:09:27 +0000

    added Temp

--- bbq/models.py
+++ bbq/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import expression
 from sqlalchemy.ext.compiler import compiles
 from bbq.database import Base
@@ -16,9 +17,28 @@ class Device(Base):
     mac = Column(String, unique=True)
     created = Column(DateTime, nullable=False, default=utcnow())
     last_modified = Column(DateTime, onupdate=utcnow())
+    temps = relationship("Temp", primaryjoin="Temp.device_id==Device.id")
 
     def __init__(self, mac=None):
         self.mac = mac
 
     def __repr__(self):
         return '<Device {}>'.format(self.mac)
+
+class Temp(Base):
+    __tablename__ = 'temps'
+    id = Column(Integer, primary_key=True)
+    temp_1 = Column(Integer, nullable=False)
+    temp_2 = Column(Integer, nullable=False)
+    volts = Column(Float, nullable=False)
+    device_id = Column(Integer, ForeignKey('devices.id'), nullable=False)
+    device = relationship(Device, primaryjoin=device_id == Device.id)
+
+    def __init__(self, device_id=None, temp_1=None, temp_2=None, volts=None):
+        self.device_id = device_id
+        self.temp_1 = temp_1
+        self.temp_2 = temp_2
+        self.volts = volts
+
+    def __repr__(self):
+        return '<Temp {} - {}, {}>'.format(self.device.mac, self.temp_1, self.temp_2)
