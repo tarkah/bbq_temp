aaf9f64b4dd12c5a9f1d9a62b7ca9e535b3344d0
Cory <cforsstrom18@gmail.com>
2019-03-22 12:45:05 -0700

    test the model!

---
+++ tests/models.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append("..") 
+
+from bbq.database import init_db
+
+init_db()
+
+
+from bbq.database import db_session
+from bbq.models import Device, Session, Temp
+
+d = Device('AABBCCDDEEFF')
+d2 = Device('FFEEDDCCBBAA')
+db_session.add(d)
+db_session.add(d2)
+db_session.commit()
+
+d = db_session.query(Device).all()[0]
+d2 = db_session.query(Device).all()[1]
+s = Session(d)
+s2 = Session(d2)
+db_session.add(s)
+db_session.add(s2)
+db_session.commit()
+
+d = db_session.query(Device).all()[0]
+d2 = db_session.query(Device).all()[1]
+s = Session(d)
+s2 = Session(d2)
+db_session.add(s)
+db_session.add(s2)
+db_session.commit()
+
+s = db_session.query(Session).all()[0]
+for x in range(0,10):
+    t = Temp(s, 150, 200, 5.55)
+    db_session.add(t)
+    db_session.commit()
+
+s = db_session.query(Session).all()[1]
+for x in range(0,10):
+    t = Temp(s, 150, 200, 5.55)
+    db_session.add(t)
+    db_session.commit()
+
+sessions = db_session.query(Session).all()
+print(sessions)
+
+devices = db_session.query(Device).all()
+print(devices)
+
+temps = db_session.query(Temp).all()
+print(temps)
